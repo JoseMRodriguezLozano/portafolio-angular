40aac961d2665faa09df0080bef71657cef89da7
José Rodríguez <jose.rodriguez94@upr.edu>
2022-08-21 01:18:07 -0400

    Fin sección 5.

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,20 +10,30 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) { 
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
-        
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    })
+  }
 
-      })
+  private cargarEquipo() {
+    this.http.get('https://angular-html-29b07-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
 
-
-
-
+      this.equipo = resp;
+      console.log(resp);
+    })
   }
 
 
